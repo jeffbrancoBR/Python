93d5772872c40400432193f4c1a59c0a9489793e
VISHNU K BALACHANDRAN <qavishnukb@gmail.com>
2024-04-11 15:46:09 +0530

    Print my name

--- Ch2 - Basics/classes_start.py
+++ Ch2 - Basics/classes_start.py
@@ -2,4 +2,6 @@
 # Example file for working with classes
 # LinkedIn Learning Python course by Joe Marini
 #
-
+print("Hello world")
+name=input("Enter your name: ")
+print("Hello", name,"nice to meet you..!")

f1f1e4eb1ccd027feb4ab6ee06591ce5efad260b
JuanRodFAU <157532708+JuanRod0312@users.noreply.github.com>
2024-07-16 12:22:39 -0400

    test commit

--- Ch2 - Basics/functions_start.py
+++ Ch2 - Basics/functions_start.py
@@ -1,11 +1,13 @@
 #
 # Example file for working with functions
 # LinkedIn Learning Python course by Joe Marini
+#edited by: 2021-07-07 Juan Rodriguez
+#test 
 #
 
 
 # TODO: define a basic function
-
+print("Hello World!")
 
 # TODO: function that takes arguments
 

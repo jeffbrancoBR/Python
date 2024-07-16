40635a6cf600874bead551d3e605dad22c504d65
JuanRodFAU <157532708+JuanRod0312@users.noreply.github.com>
2024-07-16 11:49:19 -0400

    lesson 1 hello world complete.

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -1,6 +1,16 @@
 #
 # Example file for HelloWorld
 # LinkedIn Learning Python course by Joe Marini
+#edited by: 2021-07-07 Juan Rodriguez
 #
+
+
+def main():
+    print("hello world!")
+    name = input("please enter your name: ")
+    print("nice to meet you ", name)
+
+if __name__ == "__main__":
+    main()
 
 

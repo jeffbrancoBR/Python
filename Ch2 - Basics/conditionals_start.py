d06f7872765d89d2ce4d042491004f62acf88954
JuanRodFAU <157532708+JuanRod0312@users.noreply.github.com>
2024-07-17 13:24:24 -0400

    completed conditional sturct lesson

--- Ch2 - Basics/conditionals_start.py
+++ Ch2 - Basics/conditionals_start.py
@@ -1,19 +1,38 @@
 #
 # Example file for working with conditional statements
 # LinkedIn Learning Python course by Joe Marini
+# edited by Juan Rodriguez 7/17/24
 #
 
 
 
 def main():
-    x, y = 10, 100
+    x, y = 100, 100
 
     # conditional flow uses if, elif, else
+    if x<y:
+        result="x is less than y"
+    elif x==y:
+        result="x is the same as y" 
+    else:
+        result="x is greater than y"
+    
+    print(result)
 
     # conditional statements let you use "a if C else b"
+    result = "x is less than y" if x<y else "x is greater than or the same as y"
 
     # match-case makes it easy to compare multiple values
     value = "one"
+    match value:
+        case "one":
+            print("this is one")
+        case "two":
+            print("this is two")
+        case "three" | "four":
+            print("this is three or four")
+        case _:
+            print("this is something else")
 
 if __name__ == "__main__":
     main()
